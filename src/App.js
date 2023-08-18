23eebae239a40a9a83be121bcfc6c4595e32ea37
syng828 <syng828@gmail.com>
2023-08-18 16:33:36 -0700

    updated package.json

--- src/App.js
+++ src/App.js
@@ -1,17 +1,17 @@
 import './App.css';
-import {HashRouter, Routes, Route} from "react-router-dom"
+import {BrowserRouter, Routes, Route} from "react-router-dom"
 import Home from './Pages/Home';
 import SingleBreed from './Pages/SingleBreed';
 
 function App() {
   return (
    <>
-   <HashRouter basename={process.env.PUBLIC_URL}>
+   <BrowserRouter>
    <Routes> 
     <Route path = "/" element = {<Home/>}></Route>
     <Route path = "/:name" element = {<SingleBreed/>}></Route>
    </Routes>
-   </HashRouter>
+   </BrowserRouter>
    </>
   );
 }
