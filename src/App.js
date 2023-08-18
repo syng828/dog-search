d90f24cc84a3cbb71837148f2122b591fb89cdac
syng828 <syng828@gmail.com>
2023-08-18 16:18:12 -0700

    redeployment fix test

--- src/App.js
+++ src/App.js
@@ -1,17 +1,17 @@
 import './App.css';
-import {BrowserRouter, Routes, Route} from "react-router-dom"
+import {HashRouter, Routes, Route} from "react-router-dom"
 import Home from './Pages/Home';
 import SingleBreed from './Pages/SingleBreed';
 
 function App() {
   return (
    <>
-   <BrowserRouter>
+   <HashRouter basename={process.env.PUBLIC_URL}>
    <Routes> 
     <Route path = "/" element = {<Home/>}></Route>
     <Route path = "/:name" element = {<SingleBreed/>}></Route>
    </Routes>
-   </BrowserRouter>
+   </HashRouter>
    </>
   );
 }
