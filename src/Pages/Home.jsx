6043a4d43a0e2d66cda5e6f01b560313b44c7fe4
syng828 <syng828@gmail.com>
2023-08-18 15:18:57 -0700

    Updated
    Website updated to include code.

---
+++ src/Pages/Home.jsx
@@ -0,0 +1,70 @@
+import React from 'react'
+import { useState, useEffect } from 'react'
+import { Link } from 'react-router-dom';
+
+export default function Home() {
+  const[dogs, setDogs] = useState([]);
+  const[breed, setBreed] = useState('');
+
+  useEffect(()=> {
+    fetch('https://api.thedogapi.com/v1/breeds/')
+    .then(res => res.json())
+    .then(json => setDogs(json))
+    .catch((error) => { 
+      console.log(error)})
+  }
+  ,[]) 
+
+  const searchDog = () =>  { 
+    fetch(`https://api.thedogapi.com/v1/breeds/search?q=${breed}`)
+    .then(res => res.json())
+    .then(json => setDogs(json))
+    .catch((error) => { 
+      console.log(error)})
+  }
+
+  const handleSubmit = (e) => { 
+    e.preventDefault(); 
+    searchDog();
+  }
+
+  return (
+    <div className = 'home-body'>
+      <div className = "top-half">
+      <div className = "title-bar">
+      <i class="fa-solid fa-paw"></i>
+      <h1>Dog Search</h1>
+      <i class="fa-solid fa-paw"></i>
+      </div>
+
+      <p className = "credit">API provided by <a href = "https://www.thedogapi.com/">The Dog API</a> </p>
+      <form onSubmit = {handleSubmit}>
+        <input
+        type = "text" 
+        value = {breed}
+        placeholder = "Search here to filter by breed"
+        onChange = {(e) => setBreed(e.target.value)}
+        />
+      </form>
+      </div>
+
+      {!dogs ? (<h1>Loading...</h1>): 
+        (<div className = "bottom-half">
+        {dogs.map ((dog) => (
+          <Link to ={`/${dog.name}`} key = {dog.id} style = {{ color: 'inherit', textDecoration: 'inherit'}}>
+            <article>
+              <h3 className = "dog-header">{dog.name}</h3>
+              <img src = {`https://cdn2.thedogapi.com/images/${dog.reference_image_id}.jpg`} 
+                alt = {dog.name}
+                onError={({ currentTarget }) => {
+                currentTarget.onerror = null; // prevents looping
+                currentTarget.src=`https://cdn2.thedogapi.com/images/${dog.reference_image_id}.png`;
+              }}></img> 
+            </article> </Link>
+        ))}
+      </div>
+        )
+      }
+    </div>
+  )
+}
---
+++ src/Pages/SingleBreed.jsx
@@ -0,0 +1,46 @@
+import React, { useEffect, useState } from 'react'
+import { useParams } from 'react-router-dom';
+
+export default function SingleBreed() {
+  const[breed, setBreed] = useState([]); 
+  const{name} = useParams(); 
+
+  useEffect(() => {
+    fetch(`https://api.thedogapi.com/v1/breeds/search?q=${name}`)
+    .then(res => res.json())
+    .then(json => setBreed(json))
+    .catch((error) => { 
+      console.log(error)})
+  }, [name])
+  
+
+  return ( 
+    <div>{breed.map ((b) => (
+      <section>
+        <div className = "left-half">
+          <img className = 'big-picture' src = {`https://cdn2.thedogapi.com/images/${b.reference_image_id}.jpg`} 
+      onError={({ currentTarget }) => {
+        currentTarget.onerror = null; // prevents looping
+        currentTarget.src=`https://cdn2.thedogapi.com/images/${b.reference_image_id}.png`;
+    }}></img>
+      </div>
+
+      <div className = "right-half">
+      <h1 className = "name">{b.name}</h1>
+      <div className = "text">
+        <p>Weight: {b.weight.imperial} lb</p>
+        <p>Height: {b.height.imperial} in</p>
+        <p>Bred for: {b.bred_for}</p>
+        <p>Breed Group: {b.breed_group}</p>
+        <p>Life Span: {b.life_span}</p>
+        <p>Temperament: {b.temperament}</p>
+        <p>{b.description}</p>
+      </div>
+      </div>
+
+      <p className = "credit-2">Image by rawpixel.com on Freepik</p>
+
+    </section>
+    ))}</div>
+  )
+}
